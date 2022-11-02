5eaff1b66d33611b079a8e10a522e4455d90b82f
kosarc <petiakraj@gmail.com>
2022-11-02 21:35:49 +0200

    imported json data to home component

--- src/Home.js
+++ src/Home.js
@@ -1,51 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 import "./styles/Home.css";
 
 function Home() {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "About React",
-      body: "some text about React...",
-      author: "Matt",
-      id: 0,
-    },
-    {
-      title: "About React Native",
-      body: "some text about React Native...",
-      author: "Mike",
-      id: 1,
-    },
-    {
-      title: "About React Hooks",
-      body: " some text about React Hooks...",
-      author: "Serhii",
-      id: 2,
-    },
-    {
-      title: "About React Native Navigation",
-      body: "some text about React Native navigation...",
-      author: "Mike",
-      id: 3,
-    },
-  ]);
-  let sortedBlog = blogs.filter((blogs) => blogs.author !== "Mike");
+  const [blogs, setBlogs] = useState(null);
+
   const handleClick = (itemIndex) => {
     setBlogs((prevBlog) => {
       return prevBlog.filter((item) => item.indexOf(blogs) !== itemIndex);
     });
   };
+  useEffect(() => {
+    fetch("http://localhost:8000/blog")
+      .then((response) => response.json())
+      .then((data) => setBlogs(data));
+  }, []);
 
-  return (
-    <div className="Home">
-      All authors:
-      <BlogList blogs={blogs} handleClick={handleClick} />
-      <br />
-      <div>
-        Filtered authors:
-        <BlogList blogs={sortedBlog} handleClick={handleClick} />
+  if (blogs) {
+    let sortedBlog = blogs.filter((blogs) => blogs.author !== "Mike");
+    return (
+      <div className="Home">
+        All authors:
+        <BlogList blogs={blogs} handleClick={handleClick} />
+        <br />
+        <div>
+          Filtered authors:
+          <BlogList blogs={sortedBlog} handleClick={handleClick} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
 export default Home;
